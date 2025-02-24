4d85f0f771a31c4a8a524a4f24a059084242e278
Briankibunja <ebt1.12326.24@student.tharaka.ac.ke>
2025-02-24 23:53:44 +0300

    Add files via upload

---
+++ odd and -ve.py
@@ -0,0 +1,13 @@
+start= 1
+end = 100
+for i in range(start, end):
+    if i % 2==0:
+        result= i+1 
+        print(result)
+
+start=-100
+end=0
+
+for i in range(start,end):
+    if i<0:
+        print(i)
