c049aeb2d18562424ce9f2d86b28f628c3fffad2
Briankibunja <ebt1.12326.24@student.tharaka.ac.ke>
2025-02-25 21:20:48 +0300

    Update brian.py

--- brian.py
+++ brian.py
@@ -30,3 +30,18 @@ elif choice==3:
 
 else:
      print("invalid opton select option 1 or 2")
+
+#negative and odd numbers
+start= 1
+end = 100
+for i in range(start, end):
+    if i % 2==0:
+        result= i+1 
+        print(result)
+
+start=-100
+end=0
+
+for i in range(start,end):
+    if i<0:
+        print(i)
